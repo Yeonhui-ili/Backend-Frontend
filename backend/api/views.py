89bb68d667f5970d0aa1d353bf0135754b32b4da
Yeonhui-ili <yeonhui@ewhain.net>
2024-05-13 19:11:09 +0900

    feat:Develop with separate front-end and back-end architecture

---
+++ backend/api/views.py
@@ -0,0 +1,8 @@
+from rest_framework import viewsets
+from .serializers import FoodSerializer
+from .models import Food
+# Create your views here.
+
+class FoodViewSet(viewsets.ModelViewSet):
+    queryset=Food.objects.all()
+    serializer_class=FoodSerializer
